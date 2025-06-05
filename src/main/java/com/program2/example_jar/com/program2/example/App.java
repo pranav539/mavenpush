bfa4e07a7736ce3982141062403558d85f5c6e4a
zeeyan <purushotham2628@gmail.com>
2025-06-05 14:33:23 +0530

    Azure pipeline example

---
+++ src/main/java/com/program2/example_jar/com/program2/example/App.java
@@ -0,0 +1,10 @@
+package com.program2.example_jar.com.program2.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
